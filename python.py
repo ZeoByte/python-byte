572b2ae3da14884c1b787a98225445048675d448
Alex N <alexiscool745@gmail.com>
2020-08-18 21:45:06 +0300

    Create python.py
    main code, commit

---
+++ python.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+from tkinter import messagebox
+
+window = Tk()
+
+window.title("PythonByte")
+
+window.geometry('500x350')
+
+window.iconbitmap('example.ico')
+
+def clicked():
+
+    messagebox.showinfo('True', 'You have clicked the messagebox!')
+
+btn = Button(window,text='‏‏‎ ‎‏‏‎ ‎‏‏‎‏‏‎ ‎‏‏‎ ‎ ‎‏‏‎ Click‏‏‎ ‎‏‏‎ ‎‏‏‎‏‏‎ ‎‏‏‎ ‎ ‎‏‏‎ ', command=clicked)
+
+btn.grid(column=0,row=0)
+
+btn.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+lbl = Label(window, text="from github.com/ZeoByte")
+
+lbl.grid(column=0, row=0)
+
+lbl.place(relx=1.0, rely=1.0, anchor=SE)
+
+window.mainloop()
